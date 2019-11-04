b226f1554b452a0e0b863b29d9ce87fd7ae1d539
Bruno Alla <alla.brunoo@gmail.com>
2019-11-04 12:55:46 +0000

    Remove hardcoded notification ID

--- js/background.js
+++ js/background.js
@@ -1,6 +1,8 @@
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
   if (request.type === 'semaphoreci-notifier') {
-    chrome.notifications.create('semaphoreci-notifier', request.options);
+    chrome.notifications.create(request.options, function callback(notificationId) {
+      console.log(`Callaback for ${notificationId}`);
+    });
   }
   return true;
 });
