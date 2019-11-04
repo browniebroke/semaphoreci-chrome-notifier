d363403779c19d5a8a41372d5a55558a63a46c28
Bruno Alla <alla.brunoo@gmail.com>
2019-11-04 12:24:52 +0000

    Return true in background.js

--- js/background.js
+++ js/background.js
@@ -2,5 +2,5 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
   if (request.type === 'semaphoreci-notifier') {
     chrome.notifications.create('semaphoreci-notifier', request.options);
   }
-  sendResponse();
+  return true;
 });
